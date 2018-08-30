ade78b8645318fa917f5d3b16df6b4e8ffe4cd48
Caleb Maclennan <caleb@alerque.com>
2018-08-30 16:51:29 +0300

    Use CAPS for environment variables

--- etckeeper-packages.sh
+++ etckeeper-packages.sh
@@ -3,10 +3,10 @@
 set -e
 
 : ${CFGDIR=/etc/etckeeper-packages}
-: ${pacman=$(which pacman)}
+: ${PACMAN=$(which pacman)}
 
 cd ${CFGDIR}
 
-${pacman} -Qqen > native.list
-${pacman} -Qqem > foreign.list
-${pacman} -Qqet > explicit.list
+${PACMAN} -Qqen > native.list
+${PACMAN} -Qqem > foreign.list
+${PACMAN} -Qqet > explicit.list
